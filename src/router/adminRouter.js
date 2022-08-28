f250e17a92a86794d7497473306aa86d2459059f
leimin <1047701973@qq.com>
2022-08-28 17:47:58 +0800

    Adding token for other device

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -8,6 +8,8 @@ adminRouter.post("/login", (req, res) => {
     domain: "localhost",
     maxAge: 7 * 24 * 3600 * 1000, // 注意：这里是毫秒数
   });
+  // 针对除了浏览器之外的其他设备，如何得到认证
+  res.header('authorization', value);
   res.status(200).json({
     message: "success",
   });
