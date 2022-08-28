4f89e6286f5e499fdc967e7010b7113b3613d460
leimin <1047701973@qq.com>
2022-08-28 17:42:14 +0800

    use cookie-parser

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path");
 const staticRoot = path.resolve(__dirname, "./public");
+const cookieParser = require("cookie-parser");
 // app 实际上是一个函数，用于处理请求的函数
 const app = express(); // 创建一个express应用
 const studentRouter = require("./src/router/studentRouter");
 const adminRouter = require("./src/router/adminRouter");
+
+// 加入 cookie-parse 中间件
+// cookieParser 是一个函数，这个函数调用，会返回一个中间件
+// 加入这个中间件之后，会在req对象中，注入一个cookies属性，用于获取所有请求传递过来的cookies
+// 加入这个中间件之后，会在所有res对象中，注入cookie方法，用于设置cookie
+app.use(cookieParser());
 /**
  * 下面这段代码的作用：
  * 当请求时，会根据请求的路径，从指定目录中寻找是否存在该文件，如果存在，直接响应文件内容，而不再移交给后续的处理函数
--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -3,10 +3,11 @@ const adminRouter = express.Router();
 
 adminRouter.post("/login", (req, res) => {
   const { loginId, loginPwd } = req.body;
-  res.header(
-    "set-cookie",
-    "token=23442; path=/; domain=localhost; max-age=3600"
-  );
+  res.cookie("token", "123456", {
+    path: "/",
+    domain: "localhost",
+    maxAge: 7 * 24 * 3600 * 1000, // 注意：这里是毫秒数
+  });
   res.status(200).json({
     message: "success",
   });
